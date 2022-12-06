ae16c9e7af4af97ed043864b780a4954f8f30673
Marek Piotrowski <marekpiotrowski2@icpt.pl>
2022-12-06 13:18:35 +0100

    Added MinimalApi and Swagger

--- trunk/MinimalAPIDemoApp/MinimalAPIDemo/Program.cs
+++ trunk/MinimalAPIDemoApp/MinimalAPIDemo/Program.cs
@@ -1,9 +1,14 @@
+using DataAccess.DbAccess;
+using MinimalAPIDemo;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddSingleton<IUserData, UserData>();
 
 var app = builder.Build();
 
@@ -15,5 +20,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.ConfigureApi();
 
 app.Run();
